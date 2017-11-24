9698794c613692dc0700c5b3c218b33a1b9a7f03
Aidan Hakimian <aidanhak@gmail.com>
2017-11-24 12:12:20 -0500

    More student functions

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,46 @@
-function writeStudentData(sid, semail, sname) {
+function writeStudentData(sid, semail, sname, smajor, gpa, year) {
 	firebase.database().ref('students/' + sid).set({
+		sid: sid,
 		email: semail,
 		name: sname,
+		major: smajor,
+		gpa: gpa,
+		year: year
 	});
 }
 
-function createNewUser(email, password) {
+function setGPA(gpa) {
+	firebase.database().ref('students/' + sid).set({
+		gpa: gpa
+	});
+}
+
+function changeMajor(major) {
+	firebase.database().ref('students/' + sid).set({
+		major: major
+	});
+}
+
+function writeProfessorData(pid, pemail, pname, pdept) {
+	firebase.database().ref('professor/' + pid).set({
+		pid: pid,
+		email: pemail,
+		name: pname,
+		department: pdept
+	});
+}
+
+// Registration page
+function createNewStudent(email, password, name, major, ) {
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 		// Handle Errors here.
 		var errorCode = error.code;
 		var errorMessage = error.message;
 	});
+	writeStudentData('sid', email, )
 }
 
+// Login page
 function signInExistingUser(email, password) {
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 		// Handle Errors here.
