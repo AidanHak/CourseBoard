20c9dec2cbae161f9d8b51154418ff49050ff8bf
Aidan Hakimian <aidanhak@gmail.com>
2017-11-24 11:19:11 -0500

    Move custom JS to functions.js
    Add custom JS embed to few pages

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+function writeStudentData(sid, semail, sname) {
+	firebase.database().ref('students/' + sid).set({
+		email: semail,
+		name: sname,
+	});
+}
+
+function signInExistingUser(email, password) {
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+	});
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		// User is signed in.
+		var displayName = user.displayName;
+		var email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var isAnonymous = user.isAnonymous;
+		var uid = user.uid;
+		var providerData = user.providerData;
+		// ...
+	} else {
+		// User is signed out.
+		// ...
+	}
+});
