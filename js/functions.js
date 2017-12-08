95c5fccfa1826f8549f86543d000dda7ab315a4d
Aidan Hakimian <aidanhak@gmail.com>
2017-12-07 20:22:55 -0500

    Restructure DB + documentation

--- js/functions.js
+++ js/functions.js
@@ -36,39 +36,25 @@ function initialCheck() {
 	});
 }
 
-function writeStudentData(sid, semail, sname, smajor, gpa, year) {
-	firebase.database().ref('students/' + sid).set({
-		sid: sid,
+/*
+ ** Helper function to:
+ ** createNewStudent
+ */
+function writeStudentData(semail, sname) {
+	firebase.database().ref('students/' + getUID()).set({
 		email: semail,
-		name: sname,
-		major: smajor,
-		gpa: gpa,
-		year: year
+		name: sname
 	});
 }
 
-function setGPA(gpa) {
-	firebase.database().ref('students/' + sid).set({
-		gpa: gpa
-	});
-}
-
-function changeMajor(major) {
-	firebase.database().ref('students/' + sid).set({
-		major: major
-	});
-}
-
-function writeProfessorData(pid, pemail, pname, pdept) {
-	firebase.database().ref('professor/' + pid).set({
-		pid: pid,
-		email: pemail,
-		name: pname,
-		department: pdept
-	});
-}
-
-// Registration page
+/*
+ ** Function purpose:
+ ** Registration - register new student
+ ** register.html
+ **
+ ** Required input:
+ ** Email, password, name
+ */
 function createNewStudent(email, password, name) {
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 		// Handle Errors here.
@@ -76,22 +62,17 @@ function createNewStudent(email, password, name) {
 		var errorMessage = error.message;
 		alert(errorMessage);
 	});
-	var sid = Math.floor(Math.random() * 9000000) + 1000000;
-	writeStudentData(sid, email, name, '', '', '');
+	writeStudentData(email, name);
 }
 
-// Professor will not be created by registering, only from backend/DB
-/*function createNewProfessor(email, password, name, department) {
-	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-	});
-	var pid = Math.floor(Math.random() * 9000000) + 1000000;
-	writeProfessorData(pid, email, name, department);
-}*/
-
-// Login page
+/*
+ ** Function purpose:
+ ** Login - authenticate existing user (student / professor)
+ ** login.html
+ **
+ ** Required input:
+ ** Email, password
+ */
 function signInExistingUser(email, password) {
 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
 		// Handle Errors here.
@@ -101,6 +82,13 @@ function signInExistingUser(email, password) {
 	});
 }
 
+/*
+ ** Function purpose:
+ ** Logout - sign out existing user (student / professor)
+ **
+ ** Output:
+ ** Redirect to login.html?logged_out
+ */
 function signOutUser() {
 	firebase.auth().signOut().then(function() {
 		// Sign-out successful.
@@ -111,6 +99,13 @@ function signOutUser() {
 	});
 }
 
+/*
+ ** Function purpose:
+ ** Verification - send the user an email to verify their account
+ **
+ ** Output:
+ ** Alert
+ */
 function sendEmailVerification() {
 	firebase.auth().currentUser.sendEmailVerification().then(function() {
 		// Email sent.
@@ -120,6 +115,16 @@ function sendEmailVerification() {
 	});
 }
 
+/*
+ ** Function purpose:
+ ** Reset password
+ **
+ ** Required input:
+ ** Email
+ **
+ ** Output:
+ ** Alert
+ */
 function sendPasswordReset(emailAddress) {
 	firebase.auth().sendPasswordResetEmail(emailAddress).then(function() {
 		// Email sent.
@@ -129,17 +134,19 @@ function sendPasswordReset(emailAddress) {
 	});
 }
 
-function updateName($name) {
-	firebase.auth().currentUser.updateProfile({
-		displayName: $name
-	}).then(function() {
-		// Update successful.
-		alert('Your name has successfully been changed to ' + $name + '!');
-	}).catch(function(error) {
-		// An error happened.
-	});
-}
-
+/*
+ ** Function purpose:
+ ** User data - access information about the existing user
+ ** i.e. email, name, uid (user ID)
+ */
 function getEmail() {
 	return firebase.auth().currentUser.email;
+}
+
+function getName() {
+	return firebase.auth().currentUser.displayName;
+}
+
+function getUID() {
+	return firebase.auth().currentUser.uid;
 }
