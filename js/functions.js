f1aa5c4b44dd0752615687c09d95e0b1a01b1e14
Aidan Hakimian <aidanhak@gmail.com>
2017-12-10 15:46:54 -0500

    Disable expired assignments

--- js/functions.js
+++ js/functions.js
@@ -132,7 +132,12 @@ function getAssignments(cid) {
 	$('h1.page-header').text('Assignments');
 	dbResult('/assignments/', function(key, value) {
 		if (value['course'] === cid) {
-			$('#page-wrapper').append('<div class="row assignment"><div class="col-lg-12"><div class="panel panel-default"><div class="panel-heading"><strong><a href="courses.html?cid=' + cid + '&page=assignments&aid=' + key + '">' + value['title'] + '</a></strong></div><div class="panel-body">' + value['description'] + '</div><div class="panel-footer"><span style="font-size:smaller;">Due on <span class="assignment_duedate">' + new Date(value['dueDate'] * 1000) + '</span></span></div></div></div>');
+			var dueDate = new Date(value['dueDate'] * 1000);
+			if (new Date() > dueDate) {
+				$('#page-wrapper').append('<div class="row assignment"><div class="col-lg-12"><div class="panel panel-default"><div class="panel-heading"><strong><a href="courses.html?cid=' + cid + '&page=assignments&aid=' + key + '">' + value['title'] + '</a></strong></div><div class="panel-body">' + value['description'] + '</div><div class="panel-footer"><span style="font-size:smaller;">Due on <span class="assignment_duedate">' + new Date(value['dueDate'] * 1000) + '</span></span></div></div></div>');
+			} else {
+				$('#page-wrapper').append('<div class="row assignment"><div class="col-lg-12"><div class="panel panel-default"><div class="panel-heading"><strong>' + value['title'] + '</strong></div><div class="panel-body">' + value['description'] + '</div><div class="panel-footer"><span style="font-size:smaller;">Due on <span class="assignment_duedate">' + new Date(value['dueDate'] * 1000) + '</span></span></div></div></div>');
+			}
 		}
 	}, function() {
 		// Callback to retrieving DB data
