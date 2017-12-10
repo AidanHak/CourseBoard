b03a7011bd1d6bd01384cddb62889816c555ac31
Aidan Hakimian <aidanhak@gmail.com>
2017-12-10 15:22:15 -0500

    Debugging

--- js/functions.js
+++ js/functions.js
@@ -145,6 +145,7 @@ function getOneAssignment(aid, cid, uid) {
 		if (aid === key) {
 			$('#page-wrapper').append('<div class="row assignment"><div class="col-lg-12"><div class="panel panel-default"><div class="panel-heading"><strong>' + value['title'] + '</strong></div><div class="panel-body">' + value['description'] + '<br /><form id="assignment_form"><textarea class="form-control" rows="5"></textarea><button type="button" class="btn btn-default">Submit</button></form></div><div class="panel-footer"><span style="font-size:smaller;">Due on <span class="assignment_duedate">' + new Date(value['dueDate'] * 1000) + '</span></span></div></div></div>');
 			var content = value['students'][getUID()];
+			console.log(content);
 			if (content !== undefined) {
 				$('#assignment_form textarea').prop('disabled', true);
 			} else {
