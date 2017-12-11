326d2ca488ac06ba4f876909299caa67d09ade6f
Aidan Hakimian <aidanhak@gmail.com>
2017-12-11 13:44:34 -0500

    Bug fixes

--- js/functions.js
+++ js/functions.js
@@ -228,8 +228,10 @@ function getTodayCourses() {
 		var today = new Date();
 		var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		var dayName = days[today.getDay()];
-		if (value['days'][dayName.toLowerCase()] && value['students'][uid]) {
-			$('#todaySchedule > div.panel-body').append('<div class="panel-group course"><div class="panel panel-default"><div class="panel-heading"><strong><a href="courses.html?cid=' + key + '">' + value['title'] + '</a></strong></div><div class="panel-body">' + value['description'] + '</div><div class="panel-footer"><span style="font-size:smaller;">From ' + value['startTime'] + ' to ' + value['endTime'] + '</span></div></div>');
+		if(value['students'] !== undefined && value['days'] !== undefined) {
+			if (value['days'][dayName.toLowerCase()] && value['students'][uid]) {
+				$('#todaySchedule > div.panel-body').append('<div class="panel-group course"><div class="panel panel-default"><div class="panel-heading"><strong><a href="courses.html?cid=' + key + '">' + value['title'] + '</a></strong></div><div class="panel-body">' + value['description'] + '</div><div class="panel-footer"><span style="font-size:smaller;">From ' + value['startTime'] + ' to ' + value['endTime'] + '</span></div></div>');
+			}
 		}
 	}, function() {
 		if ($('#todaySchedule > div.panel-body').is(':empty')) {
